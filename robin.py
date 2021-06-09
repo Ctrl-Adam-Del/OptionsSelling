72bd86e83282f0c0965e4347cbad7c475b3937da
kimardenmiller <kimardenmiller@gmail.com>
2021-06-09 16:09:07 -0700

    - Strikes between 1 & 2 standard deviations

--- robin.py
+++ robin.py
@@ -7,30 +7,33 @@ authenticate_robinhood()
 
 # my_holdings = rh.build_holdings()
 
-chain = rh.options.get_chains('TSLA')
+underlying_stock_symbol = 'tsla'
+chain = rh.options.get_chains(underlying_stock_symbol)
 # underlying = rh.get_instruments_by_symbols('tsla')
 underlying = rh.get_quotes('tsla')
-print('Last Trade: ', underlying[0]['last_trade_price'])
+underlying_last = round(float(underlying[0]['last_trade_price']), 2)
+print('Last Trade: ', underlying_last)
 
-underlying_history = pd.DataFrame.from_dict(rh.get_stock_historicals('tsla', interval='day', span='year'))
-pd.set_option('display.max_columns', None, 'display.width', 200)
-print(underlying_history)
-
+underlying_history = pd.DataFrame.from_dict(rh.get_stock_historicals(underlying_stock_symbol,
+                                                                     interval='day', span='year'))
 underlying_standard_deviation = underlying_history['close_price'].astype('float').std()
+
+pd.set_option('display.max_columns', None, 'display.width', 200)
+# print(underlying_history)
 print('underlying_standard_deviation: ', round(underlying_standard_deviation, 2))
 
-standard_deviation_1 = 'standard_deviation_1'
-print('1 StDev', standard_deviation_1)
 last_contract = get_leaps(chain)
-print(last_contract)
+# print(last_contract)
 
+optionData = pd.DataFrame.from_dict(rh.find_options_by_expiration(underlying_stock_symbol,
+                                                                  expirationDate=last_contract, optionType='put'))
+optionData['Strike'] = round(optionData.strike_price.astype('float')).astype('int32')
+optionData['Bid'] = round(optionData['bid_price'].astype('float'), 2)
+optionData['Ask'] = round(optionData['ask_price'].astype('float'), 2)
+optionData.sort_values('Strike', inplace=True)
+# print(optionData.head())
 
+optionData['OTM'] = underlying_last - optionData['Strike']
 
-# optionData = rh.find_options_by_expiration(['fb', 'aapl', 'tsla', 'nflx'],
-# optionData = rh.find_options_by_expiration('fb', expirationDate=last_contract, optionType='call')
-
-# for item in optionData:
-#     if float(item['strike_price']) == 300:
-#         print('300 found')
-    # print(' Strike', item['strike_price'], ' exp - ', item['expiration_date'], ' symbol - ',
-    #       item['chain_symbol'], ' delta - ', item['delta'], ' theta - ', item['theta'])
+strike_range = optionData[optionData.OTM.between(underlying_standard_deviation, underlying_standard_deviation * 2)]
+print(strike_range[['Strike', 'OTM', 'Bid', 'Ask']])
