2febbcae1455924dfd65591c35597910a6b719cf
kimardenmiller <kimardenmiller@gmail.com>
2021-06-10 14:54:38 -0700

    - Multiple contracts loading

--- robin.py
+++ robin.py
@@ -4,36 +4,41 @@ from robin_data import get_leaps
 from utility import authenticate_robinhood
 
 authenticate_robinhood()
+pd.set_option('display.max_columns', None, 'display.max_rows', None, 'display.width', 200)
+
+underlying_stock_symbol = 'TSLA'
+underlying = rh.get_quotes(underlying_stock_symbol)
+underlying_last = round(float(underlying[0]['last_trade_price']), 2)
+option_chains = rh.options.get_chains(underlying_stock_symbol)  # todo all puts?
 
 # my_holdings = rh.build_holdings()
-
-underlying_stock_symbol = 'tsla'
-chain = rh.options.get_chains(underlying_stock_symbol)
 # underlying = rh.get_instruments_by_symbols('tsla')
-underlying = rh.get_quotes('tsla')
-underlying_last = round(float(underlying[0]['last_trade_price']), 2)
-print('Last Trade: ', underlying_last)
 
 underlying_history = pd.DataFrame.from_dict(rh.get_stock_historicals(underlying_stock_symbol,
                                                                      interval='day', span='year'))
 underlying_standard_deviation = underlying_history['close_price'].astype('float').std()
 
-pd.set_option('display.max_columns', None, 'display.width', 200)
 # print(underlying_history)
-print('underlying_standard_deviation: ', round(underlying_standard_deviation, 2))
+print(f'{underlying_stock_symbol} Last: ${underlying_last:.2f} StdDev: {underlying_standard_deviation:.2f}')
 
-last_contract = get_leaps(chain)
-# print(last_contract)
+leaps = get_leaps(option_chains, 365)
+# print(leaps)
+# for leap in leaps:
+# leap = leaps[0]
+# print(underlying_stock_symbol, leap['contract'])
+# print(rh.find_options_by_expiration(
+#     underlying_stock_symbol, expirationDate='2022-06-17', optionType='put'))
+# exit(0)
+for leap in leaps:
+    option_data = pd.DataFrame.from_dict(rh.find_options_by_expiration(
+        underlying_stock_symbol, expirationDate=leap['contract'], optionType='put'))
+    print(leap['contract'])
+    option_data['Strike'] = round(option_data.strike_price.astype('float')).astype('int32')
+    option_data['Bid'] = round(option_data['bid_price'].astype('float'), 2)
+    option_data['Ask'] = round(option_data['ask_price'].astype('float'), 2)
+    option_data['OTM'] = underlying_last - option_data['Strike']
+    option_data.sort_values('Strike', inplace=True)
+    # print(optionData.head())
 
-optionData = pd.DataFrame.from_dict(rh.find_options_by_expiration(underlying_stock_symbol,
-                                                                  expirationDate=last_contract, optionType='put'))
-optionData['Strike'] = round(optionData.strike_price.astype('float')).astype('int32')
-optionData['Bid'] = round(optionData['bid_price'].astype('float'), 2)
-optionData['Ask'] = round(optionData['ask_price'].astype('float'), 2)
-optionData.sort_values('Strike', inplace=True)
-# print(optionData.head())
-
-optionData['OTM'] = underlying_last - optionData['Strike']
-
-strike_range = optionData[optionData.OTM.between(underlying_standard_deviation, underlying_standard_deviation * 2)]
-print(strike_range[['Strike', 'OTM', 'Bid', 'Ask']])
+    strike_range = option_data[option_data.OTM.between(underlying_standard_deviation, underlying_standard_deviation * 2)]
+    print(strike_range[['expiration_date', 'Strike', 'OTM', 'open_interest', 'Bid', 'Ask']])
--- robin_data.py
+++ robin_data.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 
-def get_leaps(chain):
-    contract_date = []
+def get_leaps(chain, min_to_expire):
+    contracts = []
     for contract in chain['expiration_dates']:
         date_time_str = datetime.strptime(contract, '%Y-%m-%d')
-        contract_date = date_time_str.strftime("%Y-%m-%d")
         days_to_expire = (date_time_str - datetime.today()).days
-        if days_to_expire > 365:
-            print(contract_date, days_to_expire, 'days out')
-    return contract_date
+        if days_to_expire > min_to_expire:
+            contracts.append({'contract': date_time_str.strftime("%Y-%m-%d"), 'days_to_expire': days_to_expire})
+            print(date_time_str.strftime("%Y-%m-%d"), days_to_expire, 'days out')
+    return contracts
